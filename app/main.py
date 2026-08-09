73ff44b6e831b4b964718662899cc6ac570e4e1b
AtharvaMogre <atharvamogre050505@gmail.com>
2026-08-09 11:38:11 +0530

    Add Master SOS module

--- app/main.py
+++ app/main.py
@@ -6,12 +6,14 @@ from app.database.base import Base
 import app.models
 
 from app.routers.users import router as user_router
+from app.routers.sos import router as sos_router
 
 app = FastAPI(title="LifeLink AI API")
 
 Base.metadata.create_all(bind=engine)
 
 app.include_router(user_router)
+app.include_router(sos_router)
 
 
 @app.get("/")
